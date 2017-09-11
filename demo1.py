211757837da22f148e5b7919477432fd242db7b3
zjuchenyuan <chenyuan@zju.edu.cn>
2017-09-11 16:44:17 +0800

    [feat]add __len__ method 返回还需要处理的任务数量的不准确估计值

--- demo1.py
+++ demo1.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # coding=utf-8
 import requests
-from mpms import MultiProcessesMultiThreads
-
+from mpms import MPMS #更友好的类名称
+from time import sleep
 
 def worker(no, url):
     print("requesting", no, url)
@@ -10,43 +10,45 @@ def worker(no, url):
     with open("{}.html".format(no), "wb") as fw:  # 将内容写入文件, 这也是比较耗时的IO操作
         fw.write(r.content)
     return no, r.url, r.status_code, len(r.content), r.elapsed.total_seconds()
-
+    # 这里返回了序号, url, 请求得到的http code, 请求得到的网页长度, 请求耗时
 
 def handler(meta, no, url, status_code, length, time):
     # 其中第一个参数 meta 不是由 worker() 传入的, 其他几个跟 worker() 的返回值一一对应
     #    正如字面意思, 它包含了一些在初始化时传入的参数
-    #    在本例中, 为了演示, 传入了一个文件对象, 用于写日志
+    #    在本例中, 由主线程创建对象时meta传入了一个文件对象, 用于写日志
+    #    详情请看README中的 高级 部分
     print("Done:", no, url, status_code, length, time)
-
-    fw = meta["log_file"]
-    fw.write("{} {} {} {} {}\n".format(no, url, status_code, length, time))
-
-    meta["total_req_time"] += time  # 统计总共的耗时
-
+    meta["log_file"].write("{} {} {} {} {}\n".format(no, url, status_code, length, time))
+    meta["total_req_time"] += time # 统计总共的耗时
 
 def main():
     fw = open("log.txt", "w", encoding="utf-8")
-
-    m = MultiProcessesMultiThreads(
+    
+    m = MPMS(
         worker,
         handler,
         processes=5,
         threads_per_process=10,
-        meta={"log_file": fw, "total_req_time": 0.0},
+        meta={ # 传入meta的初始值, 可以传入文件对象, 你可以不使用它
+            "log_file":fw,
+            "total_req_time":0.0
+        },
     )
-
+    
     for i in range(1000):  # 请求1000次
         m.put([i, "http://example.com/?q=" + str(i)])
-
+    
+    while len(m)>10:
+        print("Remaining: {size}".format(size=len(m)))  # 还有多少任务需要执行的周期性提示
+        sleep(1)
+    # 在剩余任务少于10个时退出循环，等待任务全部完成
     m.join()
     fw.close()
-
+    
     return m.meta["total_req_time"]
-
 
 if __name__ == '__main__':
     from time import time
-
     start = time()
     total_req_time = main()
     spent_time = time() - start
